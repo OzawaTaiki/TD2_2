09fb52913eb7f6352cab7857ee86cfdb201774d6
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-11-04 07:56:51 +0900

    _ まっさらに

--- Project/Engine/Scene/GameScene.cpp
+++ Project/Engine/Scene/GameScene.cpp
@@ -3,11 +3,6 @@
 #include "Sprite.h"
 #include "VectorFunction.h"
 #include "MatrixFunction.h"
-#include "ParticleManager.h"
-#include "TextureManager.h"
-#include "ConfigManager.h"
-#include "Quaternion.h"
-#include "MyLib.h"
 #include <chrono>
 #include <imgui.h>
 
@@ -47,20 +42,6 @@ void GameScene::Update()
     //<-----------------------
     camera_->Update();
 
-    Quaternion rotation0 = Quaternion::MakeRotateAxisAngleQuaternion(Vector3(.71f, .71f, 0), 0.3f);
-    Quaternion rotation1 = Quaternion::MakeRotateAxisAngleQuaternion(Vector3(.71f, 0, .71f), 3.141592f);
-
-    Quaternion interpolat0 = Slerp(rotation0, rotation1, 0.0f);
-    Quaternion interpolat1 = Slerp(rotation0, rotation1, 0.3f);
-    Quaternion interpolat2 = Slerp(rotation0, rotation1, 0.5f);
-    Quaternion interpolat3 = Slerp(rotation0, rotation1, 0.7f);
-    Quaternion interpolat4 = Slerp(rotation0, rotation1, 1);
-
-    interpolat0.ShowData("interpolat0", true);
-    interpolat1.ShowData("interpolat1", true);
-    interpolat2.ShowData("interpolat2", true);
-    interpolat3.ShowData("interpolat3", true);
-    interpolat4.ShowData("interpolat4", true);
 
     trans_.UpdateData();
 
