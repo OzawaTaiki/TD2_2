d6112c2096b47f07ba180093dd0ece78d4bb56d0
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-11-14 10:48:12 +0900

    applicationフォルダを追加

---
+++ Project/Application/Enemy/Enemy.cpp
@@ -0,0 +1,188 @@
+#include <MatrixFunction.h>
+#include "VectorFunction.h"
+
+#include "Enemy.h"
+#include "Player.h"
+
+
+
+
+void Enemy::Initialize()
+{
+	worldTransform_.Initialize();
+	worldTransform_.transform_ = Vector3{ 0,0,0 };
+
+	model_ = Model::CreateFromObj("Arrow/Arrow.obj");
+
+	color_.Initialize();
+	color_.SetColor(Vector4{ 1, 1, 1, 1 });
+
+	modelBullet_ = Model::CreateFromObj("Arrow/Arrow.obj");;
+}
+
+void Enemy::Update()
+{
+	if (behaviorRequest_) {
+		// ふるまいを変更する
+		behavior_ = behaviorRequest_.value();
+		// 各ふるまいごとの初期化を実行
+		switch (behavior_) {
+		case Behavior::kRoot:
+		default:
+			BehaviorRootInitialize();
+			break;
+		case Behavior::kAttack:
+			BehaviorAttackInitialize();
+			break;
+		case Behavior::kJump:
+			//BehaviorJumpInitialize();
+			break;
+		}
+		// ふるまいリクエストリセット
+		behaviorRequest_ = std::nullopt;
+	}
+	switch (behavior_) {
+	case Behavior::kRoot: // 通常行動更新
+	default:
+		BehaviorRootUpdate();
+		break;
+	case Behavior::kAttack: // 攻撃行動更新
+		BehaviorAttackUpdate();
+		break;
+	case Behavior::kJump:
+		//BehaviorJumpUpdate(); // ジャンプ行動更新
+		break;
+	}
+	
+	BulletUpdate();
+	// HP
+	if (hp <= 0) {
+		isAlive = false;
+	}
+
+
+	// ワールドトランスフォーム更新
+	worldTransform_.UpdateData();
+}
+
+void Enemy::Draw(const Camera& camera)
+{
+	model_->Draw(worldTransform_, &camera, &color_);
+
+	for (const auto& bullet : bullets_) {
+		bullet->Draw(camera);
+	}
+
+	switch (behavior_) {
+	case Behavior::kRoot:
+	case Behavior::kJump:
+	default:
+
+		break;
+	case Behavior::kAttack:
+		break;
+	}
+}
+
+void Enemy::BulletInitialize(Vector3 pos)
+{
+	const float kBulletSpeed = 0.4f;
+	Vector3 velocityB{};
+
+	const int numBullets = 16;
+	const float angleStep = 360.0f / numBullets;
+
+	for (int i = 0; i < numBullets; i++) {
+		float angle = i * angleStep;
+		float radian = angle * (3.14f / 180.0f);  // Convert to radians
+
+		float rotate = behaviorTimer_ / 3000;
+
+		Vector3 direction{ cos(radian + behaviorTimer_), pos.y, sin(radian + behaviorTimer_) };
+
+		velocityB = Subtract(direction, pos);
+		velocityB = Multiply(Normalize(velocityB), kBulletSpeed);
+
+		// 弾を生成し、初期化
+		auto newBullet = std::make_unique<EnemyBullet>();
+		newBullet->Initialize(pos, velocityB, modelBullet_);
+
+		// 弾の親設定
+		newBullet->SetParent(worldTransform_.parent_);
+
+		// 弾を登録する
+		bullets_.push_back(std::move(newBullet));
+	}
+
+}
+
+void Enemy::BulletUpdate()
+{
+	for (const auto& bullet : bullets_) {
+		bullet->Update();
+	}
+
+	// デスフラグが立った弾を削除
+	bullets_.remove_if([](const std::unique_ptr<EnemyBullet>& bullet) { return bullet->IsDead(); });
+}
+
+void Enemy::BehaviorRootInitialize()
+{
+	// 浮遊ギミック
+	InitializeFloatingGimmick();
+}
+
+void Enemy::BehaviorRootUpdate()
+{
+	velocity_ = { 0.0f, 0.0f, 0.0f };
+	speed = 0.2f;
+
+	behaviorTimer_++;
+	if (behaviorTimer_ >= 180) {
+		behaviorTimer_ = 0;
+		behaviorRequest_ = Behavior::kAttack;
+	}
+
+	// 浮遊ギミック
+	UpdateFloatingGimmick();
+}
+
+void Enemy::BehaviorAttackInitialize()
+{
+
+}
+
+void Enemy::BehaviorAttackUpdate()
+{
+	behaviorTimer_++;
+	if (behaviorTimer_ >= 120) {
+		behaviorTimer_ = 0;
+		behaviorRequest_ = Behavior::kRoot;
+	}
+
+	if (behaviorTimer_ % 15 == 0) {
+		BulletInitialize(worldTransform_.GetWorldPosition());
+	}
+}
+
+void Enemy::InitializeFloatingGimmick()
+{
+	floatingParameter_ = 0.0f;
+}
+
+void Enemy::UpdateFloatingGimmick()
+{
+	float pi = 3.141592f;
+	// 1フレームでのパラメータ加算値
+	const float step = 2.0f * pi / floatingPeriod;
+	// パラメータを1ステップ分加算
+	floatingParameter_ += step;
+
+	// 2πを超えたら0に戻す
+	floatingParameter_ = std::fmod(floatingParameter_, 2.0f * pi);
+
+	float floatingAmplitude = 0.5f;
+
+	// 浮遊を座標に反映
+	worldTransform_.transform_.y = std::sin(floatingParameter_) * floatingAmplitude;
+}
---
+++ Project/Application/FollowCamera.cpp
@@ -0,0 +1,48 @@
+#include "FollowCamera.h"
+#include "MatrixFunction.h"
+#include "VectorFunction.h"
+#include"imgui.h"
+#include "Player.h"
+void FollowCamera::Initialize()
+{
+	camera_.Initialize();
+	camera_.rotate_ = { 0.34f,0,0 };
+}
+
+void FollowCamera::Update()
+{
+	if (ImGui::BeginTabBar("followCamera"))
+	{
+		if (ImGui::BeginTabItem("followCamera"))
+		{
+			ImGui::DragFloat3("translate", &camera_.translate_.x, 0.01f);
+			ImGui::DragFloat3("rotate", &camera_.rotate_.x, 0.01f);
+			ImGui::EndTabItem();
+		}
+		ImGui::EndTabBar();
+	}
+	if (target_) {
+		const float kRotateSpeed = 0.000003f *10000;
+		if (Input::GetInstance()->IsKeyPressed(DIK_RIGHT)) {
+			camera_.rotate_.y += 1.0f * kRotateSpeed;
+		}
+		else if (Input::GetInstance()->IsKeyPressed(DIK_LEFT)) {
+			camera_.rotate_.y -= 1.0f * kRotateSpeed;
+		}
+
+		//camera_.rotate_.y = target_->rotate_.y;
+
+		// 追従対象からカメラまでのオフセット
+		Vector3 offset = { 0.0f, 16.0f, -40.0f };
+
+		Matrix4x4 matrix = MakeRotateYMatrix(camera_.rotate_.y);
+
+		// オフセットをカメラの回転に合わせて回転させる
+		offset = Transform(offset, matrix);
+
+		// 座標をコピーしてオフセット分ずらす
+		camera_.translate_ = Add(target_->transform_, offset);
+	}
+
+	camera_.UpdateMatrix();
+}
